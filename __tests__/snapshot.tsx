75cf5038aac9509bab665e7bf13f04a593a4a0e1
Justin D. Harris <juharris@users.noreply.github.com>
2024-10-26 22:12:30 -0400

    UI: Optimize rendering to only render items in view using react-window

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -8,6 +8,7 @@ it("renders homepage unchanged", () => {
   const { container } = render(<Home />);
   // Skip because the snapshot is too big and will change often because of the new dates getting added.
   // TODO Figure out how to ignore the dates in the snapshot and re-enable this test once the UI gets more stable.
+  // Maybe we should ignore the entire solutions section because it changes as new solutions are added.
   // expect(container).toMatchSnapshot();
   // Just use do a simple test for now to make sure that it can be rendered without errors.
   expect(container).toBeTruthy();
--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,8 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return <div>
+    <h1>App Router</h1>
+    Go <a href="/home">here</a>
+  </div>
 }
